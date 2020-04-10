edd560ce8c767006eb33c012aab820a7d1b47899
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2020-04-10 16:19:28 +0900

    ・import文を最適化する。

--- app/src/main/java/jp/co/myself/psystem_android/utils/retrofit/api/PSystemWebService.java
+++ app/src/main/java/jp/co/myself/psystem_android/utils/retrofit/api/PSystemWebService.java
@@ -2,7 +2,6 @@ package jp.co.myself.psystem_android.utils.retrofit.api;
 
 import io.reactivex.Observable;
 import jp.co.myself.psystem_android.utils.retrofit.entity.ResultIssueJWTForSignup;
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
